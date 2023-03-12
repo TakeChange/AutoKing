9e24bf3a2496d47fe31caa9099f94b73d37db762
vaishnavi9491 <vaishnavijadhav5279@gmail.com>
2023-03-12 13:03:59 +0530

    pull changes

--- src/Frames/Addcustomer.java
+++ src/Frames/Addcustomer.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Frames;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 /**
  *
  * @author vaish
@@ -35,12 +37,12 @@ public class Addcustomer extends javax.swing.JFrame {
         jLabel18 = new javax.swing.JLabel();
         jLabel19 = new javax.swing.JLabel();
         jLabel20 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        cust1 = new javax.swing.JTextField();
+        cust2 = new javax.swing.JTextField();
+        cust3 = new javax.swing.JTextField();
+        cust4 = new javax.swing.JTextField();
+        cust5 = new javax.swing.JTextField();
+        save = new javax.swing.JButton();
         jLabel21 = new javax.swing.JLabel();
         t1 = new javax.swing.JLabel();
         t2 = new javax.swing.JLabel();
@@ -52,6 +54,9 @@ public class Addcustomer extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        cust6 = new com.toedter.calendar.JDateChooser();
+        t6 = new javax.swing.JLabel();
+        clear = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
 
@@ -95,36 +100,36 @@ public class Addcustomer extends javax.swing.JFrame {
         jLabel20.setText("Address                       :");
         jPanel2.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 470, -1, -1));
 
-        jTextField1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jPanel2.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 170, 260, 30));
+        cust1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jPanel2.add(cust1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 170, 260, 30));
 
-        jTextField2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jPanel2.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 260, 30));
+        cust2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jPanel2.add(cust2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 260, 30));
 
-        jTextField3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jPanel2.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 310, 260, 30));
+        cust3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jPanel2.add(cust3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 310, 260, 30));
 
-        jTextField4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+        cust4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        cust4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
+                cust4ActionPerformed(evt);
             }
         });
-        jPanel2.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 390, 260, 30));
+        jPanel2.add(cust4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 390, 260, 30));
 
-        jTextField5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jPanel2.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 470, 260, 30));
+        cust5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jPanel2.add(cust5, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 470, 260, 30));
 
-        jButton1.setBackground(new java.awt.Color(255, 165, 0));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("SAVE");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        save.setBackground(new java.awt.Color(255, 165, 0));
+        save.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        save.setForeground(new java.awt.Color(255, 255, 255));
+        save.setText("SAVE");
+        save.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                saveActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 540, 120, -1));
+        jPanel2.add(save, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 540, 120, -1));
 
         jLabel21.setFont(new java.awt.Font("Segoe UI Semibold", 0, 18)); // NOI18N
         jLabel21.setText("Customer  id     :");
@@ -169,6 +174,21 @@ public class Addcustomer extends javax.swing.JFrame {
         jLabel9.setForeground(new java.awt.Color(255, 0, 0));
         jLabel9.setText("*");
         jPanel2.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 380, 40, 30));
+        jPanel2.add(cust6, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 30, -1, -1));
+
+        t6.setForeground(new java.awt.Color(255, 51, 51));
+        jPanel2.add(t6, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 60, 110, 20));
+
+        clear.setBackground(new java.awt.Color(255, 165, 0));
+        clear.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        clear.setForeground(new java.awt.Color(255, 255, 255));
+        clear.setText("CLEAR");
+        clear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearActionPerformed(evt);
+            }
+        });
+        jPanel2.add(clear, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 540, 120, -1));
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 0, 580, 770));
 
@@ -184,69 +204,112 @@ public class Addcustomer extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
-    }//GEN-LAST:event_jTextField4ActionPerformed
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void cust4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cust4ActionPerformed
+    }//GEN-LAST:event_cust4ActionPerformed
+    private void saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveActionPerformed
         // TODO add your handling code here:
         boolean isValid = true;
-        String id=t1.getText().toString();
-        String cname=t2.getText().toString();
-        String mobile=t3.getText().toString();
-        String email=t4.getText().toString();
-        String addres=t5.getText().toString();
         
+
+
+
+         String id= cust1.getText();
+        
+        //For id Customer id 
         if(id.equals(""))
         {
-            t1.setText("customer id do not empty.");
-            isValid = false;
+            t1.setText("Customer id do not empty.");
         }
         else
         {
             t1.setText("");
         }
-        
-            
-            if(id.equals(""))
+        //For id Customer name 
+        String name= cust2.getText();
+        if(name.equals(""))
         {
-            t2.setText("customer name do not empty.");
-            isValid = false;
+            t2.setText("Customer name do not empty.");
         }
         else
-        {
+       {
             t2.setText("");
         }
-            
-             if(id.equals(""))
+        
+        //For id Customer Mobile
+        String mobile= cust3.getText();
+        if(mobile.equals(""))
         {
-            t3.setText("mobilenumber do not empty.");
-            isValid = false;
+            t3.setText("Mobile do not empty.");
         }
         else
         {
             t3.setText("");
         }
-             
-             if(id.equals(""))
+        
+        
+        //For id Customer Email
+        String quantity= cust4.getText();
+        if(quantity.equals(""))
         {
-            t4.setText("email do not empty.");
-            isValid = false;
+            t4.setText("Email do not empty.");
         }
         else
         {
             t4.setText("");
         }
-              if(id.equals(""))
+        
+        
+        //For id Customer Address
+        String cname= cust5.getText();
+        if(cname.equals(""))
         {
-            t5.setText("address do not empty.");
-            isValid = false;
+            t5.setText("Address do not empty.");
         }
         else
         {
             t5.setText("");
         }
         
+       //For id Date
+      
+       try
+        {
+            Date temp = cust6.getDate();
+            System.out.println("Date Format:"+temp);
+           if(temp == null)
+           {
+               t6.setText("Date do not empty.");
+            }
+            else
+            {
+                t6.setText("");
+                
+           }
+      }
+       catch(Exception e)
+       {
+           System.out.println(e);
+       }
         
-    }//GEN-LAST:event_jButton1ActionPerformed
+        
+        
+    }//GEN-LAST:event_saveActionPerformed
+
+    private void clearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearActionPerformed
+        
+               clear.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                     cust1.setText("");
+                     cust2.setText("");
+                     cust3.setText("");
+                     cust4.setText("");
+                     cust5.setText("");
+                     cust6.setCalendar(null);
+                }
+                });      // TODO add your handling code here: 
+               
+    }//GEN-LAST:event_clearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -284,7 +347,13 @@ public class Addcustomer extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton clear;
+    private javax.swing.JTextField cust1;
+    private javax.swing.JTextField cust2;
+    private javax.swing.JTextField cust3;
+    private javax.swing.JTextField cust4;
+    private javax.swing.JTextField cust5;
+    private com.toedter.calendar.JDateChooser cust6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
@@ -302,15 +371,12 @@ public class Addcustomer extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
+    private javax.swing.JButton save;
     private javax.swing.JLabel t1;
     private javax.swing.JLabel t2;
     private javax.swing.JLabel t3;
     private javax.swing.JLabel t4;
     private javax.swing.JLabel t5;
+    private javax.swing.JLabel t6;
     // End of variables declaration//GEN-END:variables
 }
